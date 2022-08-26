09293e8451ef197d67ae1e29b25e952415f1bbc1
JeffreyHuang0714 <54919510+JeffreyHuang0714@users.noreply.github.com>
2022-08-26 00:00:37 -0500

    Add files via upload

---
+++ SAM1960/Bomarc.js
@@ -0,0 +1,125 @@
+let BomarcSites = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.680833,
+          40.833333,
+          0
+        ]
+      },
+      "properties": {
+        "name": "Suffolk County AFB BOMARC Site",
+        "operator" : "6th Air Defense Missile Squadron",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.805,
+          46.348056,
+          0
+        ]
+      },
+      "properties": {
+        "name": "Kincheloe AFB BOMARC Site",
+        "operator" : "37th Air Defense Missile Squadron",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.479722,
+          37.099167,
+          0
+        ]
+      },
+      "properties": {
+        "name": "Langley AFB BOMARC Site",
+        "operator" : "22d Air Defense Missile Squadron",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.441389,
+          40.035,
+          0
+        ]
+      },
+      "properties": {
+        "name": "McGuire AFB BOMARC Site",
+        "operator" : "46th Air Defense Missile Squadron",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.539167,
+          41.682222,
+          0
+        ]
+      },
+      "properties": {
+        "name": "Otis AFB BOMARC Site",
+        "operator" : "26th Air Defense Missile Squadron",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -91.883333,
+          46.932222,
+          0
+        ]
+      },
+      "properties": {
+        "name": "Duluth Air Force Missile Site, BOMARC Site",
+        "operator" : "74th Air Defense Missile Squadron",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -68.786389,
+          44.853056,
+          0
+        ]
+      },
+      "properties": {
+        "name": "Dow AFB BOMARC Site",
+        "operator" : "30th Air Defense Missile Squadron",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -78.946944,
+          43.1175,
+          0
+        ]
+      },
+      "properties": {
+        "name": "Niagara Falls Air Force Missile Site, BOMARC Site",
+        "operator" : "35th Air Defense Missile Squadron",
+      }
+    }
+  ]
+};
